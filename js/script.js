3d224e1c3158480ccfd84483919acfbc2cc694f9
CupColdCoffee <aartfaan@mail.ru>
2020-08-16 15:30:34 +0400

    learn fetch API

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 'use strict';
+
 document.addEventListener('DOMContentLoaded', () => {
 
     //Tabs
@@ -179,34 +180,32 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    new Card (
-        'img/tabs/vegy.jpg',
-        'vegy',
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        9,
-        '.menu .container'
-    ).render();
-
-    new Card (
-        'img/tabs/elite.jpg',
-        'elite',
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        15,
-        '.menu .container',
-    ).render();
-
-    new Card (
-        'img/tabs/post.jpg',
-        'post',
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        12,
-        '.menu .container',
-    ).render();
+    getResource('http://localhost:3000/menu')
+    .then(data => {
+        data.forEach(({img, altimg, title, descr, price}) => {
+            new Card(img, altimg, title, descr, price).render();
+        });
+    });
 
     // Forms
+
+    async function getResource(url) {
+        let res = await fetch(url);
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+        return await res.json();
+    }
+
+    async function postData(url, data) {
+        let res = await fetch(url, {
+            method: 'POST',
+            headers:
+                {'Content-type': 'application/json'},
+            body: data
+        });
+        return await res.json();
+    }
 
     const forms = document.querySelectorAll('form');
     const message = {
@@ -216,10 +215,10 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     forms.forEach(item => {
-        postData(item);
+        bindPostData(item);
     });
 
-    function postData (form) {
+    function bindPostData (form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
 
@@ -229,30 +228,26 @@ document.addEventListener('DOMContentLoaded', () => {
             margin: 0 auto;
             `;
             statusMessage.src = message.loading;
-            form.insertAdjacentElement('afterend', statusMessage);
-            
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            request.setRequestHeader('Content-type', 'application/json');
+            form.insertAdjacentElement('afterend', statusMessage);            
+
             const formData = new FormData(form);
 
             const object = {};
             formData.forEach((value, key) => {
                 object[key] = value;
             });
-            const json = JSON.stringify(object);
 
-            request.send(json);
-
-            request.addEventListener('load', () => {
+            postData('server.php',JSON.stringify(object))
+            .then(data => {
                 statusMessage.remove();
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showModalMessage(message.success);
-                    form.reset();
-                } else {
-                    showModalMessage(message.failure);
-                }
+                console.log(data);
+                showModalMessage(message.success);
+            })
+            .catch(() => {
+                showModalMessage(message.failure);
+            })
+            .finally(() => {
+                form.reset();
             });
         });
     }
@@ -281,4 +276,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 4000);
     }
 
+    fetch('http://localhost:3000/menu')
+    .then(data => data.json())
+    .then(res => console.log(res));
 });
